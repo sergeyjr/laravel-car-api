1a3143f4666f2566afa7379bbe41fa1ec27cc9a8
sergeyjr <sergeyjr79@mail.ru>
2026-03-29 21:13:58 +0300

    -

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -30,11 +29,6 @@ class User extends Authenticatable
         return [
             'password' => 'hashed',
         ];
-    }
-
-    public function validatePassword(string $password): bool
-    {
-        return Hash::check($password, $this->password);
     }
 
 }
