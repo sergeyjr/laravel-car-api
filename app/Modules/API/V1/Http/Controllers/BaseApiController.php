ca5435480f5d173c789c269428301c62284ae384
sergeyjr <sergeyjr79@mail.ru>
2026-03-29 18:16:41 +0300

    -

--- app/Modules/API/V1/Http/Controllers/BaseApiController.php
+++ app/Modules/API/V1/Http/Controllers/BaseApiController.php
@@ -5,7 +5,7 @@ namespace Modules\API\V1\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Modules\API\V1\Helpers\ApiResponse;
 
-class BaseApiController extends Controller
+abstract class BaseApiController extends Controller
 {
 
     protected function success($data = null, int $code = 200)
